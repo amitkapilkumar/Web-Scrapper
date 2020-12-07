e063c67d5ffe283af1c18c1d4a41134d0551a199
Amit Kumar <amit.kapilkumar@gmail.com>
2020-12-07 17:30:20 +0000

    Init commit : Spring-boot app

---
+++ src/main/java/com/crawl/WebScrapper/WebScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.crawl.WebScrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crawl/WebScrapper/WebScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawl.WebScrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
